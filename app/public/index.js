4d4562d23d9243915f0cfc15414c05b751acc8b0
Colin Tindle <ampersand@underscore.space>
2018-06-22 16:36:57 +0100

    New cat added to cat-list Some styles

--- app/public/index.js
+++ app/public/index.js
@@ -1,11 +1,36 @@
 (() => {
-  document.querySelector('form#new-cat').addEventListener('submit', e => {
-    e.preventDefault()
-    const cat_name = e.target.cat_name.value
-    const cat_colour = e.target.cat_colour.value
+  const catList = document.querySelector('#cat-list')
 
-    console.log(cat_name, cat_colour)
+  document.querySelector('form#cat').addEventListener('submit', e => {
+    e.preventDefault()
+    const catName = e.target.cat__name.value
+    const catColour = e.target.cat__colour.value
+
+    const catNameElement = newElement('h2', 'cat__name', catName)
+    const catColourElement = newElement('p', 'cat__colour', catColour)
+    const catDeleteElement = newElement('button', 'cat__delete', 'Delete')
+    catDeleteElement.addEventListener('click', e => {
+      if (confirm('Are you sure?')) {
+        e.target.parentElement.remove()
+      }
+    })
+
+    const catListItem = newElement('div', 'cat-list__item')
+
+    catListItem.appendChild(catNameElement)
+    catListItem.appendChild(catColourElement)
+    catListItem.appendChild(catDeleteElement)
+
+    catList.appendChild(catListItem)
 
     e.target.reset()
   })
+
+  function newElement(kind, className, content) {
+    const element = document.createElement(kind)
+    element.classList.add(className)
+    if (content) { element.textContent = content}
+    return element
+  }
+
 })();
