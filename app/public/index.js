dc5a9e2ec1a4bdf5ce1a1d8279ecd4e75cc527c7
Colin Tindle <ampersand@underscore.space>
2018-06-22 18:00:26 +0100

    Some DRYing and pretty colours

--- app/public/index.js
+++ app/public/index.js
@@ -1,36 +1,47 @@
 (() => {
+  let hue = 0
   const catList = document.querySelector('#cat-list')
+  const deleteAllCats = document.querySelector('#delete-all-cats')
 
   document.querySelector('form#cat').addEventListener('submit', e => {
     e.preventDefault()
-    const catName = e.target.cat__name.value
-    const catColour = e.target.cat__colour.value
+    const {cat__name, cat__colour} = e.target
 
-    const catNameElement = newElement('h2', 'cat__name', catName)
-    const catColourElement = newElement('p', 'cat__colour', catColour)
-    const catDeleteElement = newElement('button', 'cat__delete', 'Delete')
+    const catNameElement = createElement('h2', 'cat__name', cat__name.value)
+    const catColourElement = createElement('p', 'cat__colour', cat__colour.value)
+
+    const catDeleteElement = createElement('button', 'cat__delete', 'Delete')
     catDeleteElement.addEventListener('click', e => {
       if (confirm('Are you sure?')) {
         e.target.parentElement.remove()
       }
     })
 
-    const catListItem = newElement('div', 'cat-list__item')
-
-    catListItem.appendChild(catNameElement)
-    catListItem.appendChild(catColourElement)
-    catListItem.appendChild(catDeleteElement)
-
+    const catListItem = createElement('div', 'cat-list__item')
+    catListItem.style.setProperty('--h', elementHue())
+    appendElements(catListItem, [catNameElement, catColourElement, catDeleteElement])
     catList.appendChild(catListItem)
 
-    e.target.reset()
+    // e.target.reset()
   })
 
-  function newElement(kind, className, content) {
+  function createElement(kind, className, content) {
     const element = document.createElement(kind)
     element.classList.add(className)
     if (content) { element.textContent = content}
     return element
+  }
+
+  function appendElements(parent, elementArray) {
+    for (const element of elementArray) {
+      parent.appendChild(element)
+    }
+  }
+
+  function elementHue() {
+    hue += Math.floor(360/15)
+    if (hue >= 360) { hue = 0}
+    return hue
   }
 
 })();
